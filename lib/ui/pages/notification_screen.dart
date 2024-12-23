35a227f4506748d7ce0eaa2317f5d373999b0812
AhmeBoda12 <ahmedboda98888@gmail.com>
2024-12-23 16:42:07 +0200

    init

---
+++ lib/ui/pages/notification_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class NotificationScreen extends StatefulWidget {
+  const NotificationScreen({super.key});
+
+  @override
+  NotificationScreenState createState() => NotificationScreenState();
+}
+
+class NotificationScreenState extends State<NotificationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
