ece5df24a34a5075eeb307b45ee6594a3dae24e3
AhmeBoda12 <ahmedboda98888@gmail.com>
2024-12-23 17:17:26 +0200

    set the app theme

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,23 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:to_do_app/theme/app_colors.dart';
 
-const Color bluishClr = Color(0xFF4e5ae8);
-const Color orangeClr = Color(0xCFFF8746);
-const Color pinkClr = Color(0xFFff4667);
-const Color white = Colors.white;
-const primaryClr = bluishClr;
-const Color darkGreyClr = Color(0xFF121212);
-const Color darkHeaderClr = Color(0xFF424242);
+
 
 class Themes {
   static final light = ThemeData(
-    primaryColor: primaryClr,
+    primaryColor: AppColors.primaryClr,
     brightness: Brightness.light,
   );
 
   static final dark = ThemeData(
-    primaryColor: darkGreyClr,
+    primaryColor: AppColors.darkGreyClr,
     brightness: Brightness.dark,
   );
 
@@ -26,7 +21,7 @@ class Themes {
         textStyle: TextStyle(
       fontSize: 24,
       fontWeight: FontWeight.bold,
-      color: Get.isDarkMode ? white : Colors.black,
+      color: Get.isDarkMode ? AppColors.white : Colors.black,
     ));
   }
 
@@ -35,7 +30,7 @@ class Themes {
         textStyle: TextStyle(
       fontSize: 20,
       fontWeight: FontWeight.bold,
-      color: Get.isDarkMode ? white : Colors.black,
+      color: Get.isDarkMode ? AppColors.white : Colors.black,
     ));
   }
 
@@ -44,7 +39,7 @@ class Themes {
         textStyle: TextStyle(
       fontSize: 18,
       fontWeight: FontWeight.bold,
-      color: Get.isDarkMode ? white : Colors.black,
+      color: Get.isDarkMode ? AppColors.white : Colors.black,
     ));
   }
 
@@ -53,7 +48,7 @@ class Themes {
         textStyle: TextStyle(
       fontSize: 16,
       fontWeight: FontWeight.w400,
-      color: Get.isDarkMode ? white : Colors.black,
+      color: Get.isDarkMode ? AppColors.white : Colors.black,
     ));
   }
 
@@ -62,7 +57,7 @@ class Themes {
         textStyle: TextStyle(
       fontSize: 14,
       fontWeight: FontWeight.w400,
-      color: Get.isDarkMode ? white : Colors.black,
+      color: Get.isDarkMode ? AppColors.white : Colors.black,
     ));
   }
 
